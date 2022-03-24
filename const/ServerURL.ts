21e1e7d3e8c6acdf8746d0468f4aa4346c49f2bc
Lucas <lucasalonso11800@gmail.com>
2022-03-24 17:22:23 -0300

    URL

--- const/ServerURL.ts
+++ const/ServerURL.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://tennis-world-nextjs.vercel.app/api';
+export const API_URL = 'https://tennis-world-nextjs-topaz.vercel.app/api';
 export const TENNIS_API_URL = 'https://tennis-live-data.p.rapidapi.com';
 // export const API_URL = 'http://localhost:5000';
